0d43e4496a4ba51606272ea8bd2b1d4ee30dcc97
Shazzad501 <assazzad501@gmail.com>
2024-09-18 22:42:34 +0600

    home page build up start

---
+++ home.js
@@ -0,0 +1,4 @@
+document.getElementById('add-money').addEventListener('click',function(event){
+  const addMoney = document.getElementById('add-money').value;
+  const pinNumber = document.getElementById('Pin-number').value;
+})
