e6e7117c5c14187739c77ada45a6205c8924b9c6
Shazzad501 <assazzad501@gmail.com>
2024-09-18 22:17:36 +0600

    login done

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 document.getElementById('login-btn').addEventListener('click', function(event){
   event.preventDefault();
   const phoneNumber = document.getElementById('Phone-number').value;
-  console.log(phoneNumber)
+  const pinNumber = document.getElementById('pin-number').value;
+  
+  if(phoneNumber === "123456789" || pinNumber === "12345"){
+    window.location.href = 'home.html'
+  }
+  else{
+    alert('Wrong Phone number or PIN number')
+  }
 })
 // login form section end
