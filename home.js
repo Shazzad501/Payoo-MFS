c5e63392b2ee44855b88746a8292d49dfd3768f2
Shazzad501 <assazzad501@gmail.com>
2024-09-18 23:00:51 +0600

    pin and add money collect

--- home.js
+++ home.js
@@ -1,4 +1,13 @@
 document.getElementById('add-money').addEventListener('click',function(event){
-  const addMoney = document.getElementById('add-money').value;
+  event.preventDefault();
   const pinNumber = document.getElementById('Pin-number').value;
+  if(pinNumber === '12345'){
+    const addMoney = document.getElementById('add-money').value;
+    const Balance = document.getElementById('balance').innerText;
+    Balance = Balance+addMoney;
+  }
+  else{
+    alert('Wrong PIN number.')
+  }
+  
 })
