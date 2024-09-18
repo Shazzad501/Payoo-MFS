5aafc048856aa9cc7474fb2500924c9ec65055e5
Shazzad501 <assazzad501@gmail.com>
2024-09-18 21:52:58 +0600

    login form add and Prevent deapult control set

---
+++ script.js
@@ -0,0 +1,7 @@
+// login form section start
+document.getElementById('login-btn').addEventListener('click', function(event){
+  event.preventDefault();
+  const phoneNumber = document.getElementById('Phone-number').value;
+  console.log(phoneNumber)
+})
+// login form section end
