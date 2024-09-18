f45b3be118ee4ebb459c533bea15d6b63a3a9c29
Shazzad501 <assazzad501@gmail.com>
2024-09-19 00:06:41 +0600

    Implemented cash out feature

--- home.js
+++ home.js
@@ -1,3 +1,4 @@
+// Add money js
 document.getElementById('add-money').addEventListener('click',function(event){
   event.preventDefault();
   const addAmount = document.getElementById('add-amount').value;
@@ -13,7 +14,43 @@ document.getElementById('add-money').addEventListener('click',function(event){
     document.getElementById('Pin-number').value = '';
   }
   else{
-    alert('Wrong PIN, faild to add money. Please try again!')
+    alert('Faild to add money. Please try again!')
   }
   
 })
+
+// cash out js
+
+document.getElementById('cash-out').addEventListener('click',function(event){
+  event.preventDefault();
+  const cashOutAmount = document.getElementById('cash-out-amount').value;
+  const cashOutPin = document.getElementById('cash-out-pin').value;
+
+  if(cashOutPin === '12345'){
+    const Balance = document.getElementById('balance').innerText;
+    const BalanceConvNumber = parseFloat(Balance);
+    const cashOutAmountConvNumber = parseFloat(cashOutAmount);
+    const newBalance = BalanceConvNumber - cashOutAmountConvNumber;
+    document.getElementById('balance').innerText = newBalance;
+    document.getElementById('cash-out-amount').value = '';
+    document.getElementById('cash-out-pin').value = '';
+  }
+  else{
+    alert('Faild to Cash Out. Please try again!')
+  }
+  
+})
+
+// add money button js
+
+document.getElementById('money-add').addEventListener('click', function(){
+  document.getElementById('add-money-form').style.display = 'block';
+})
+
+// cash out button js
+
+document.getElementById('money-out').addEventListener('click', function(){
+  document.getElementById('cash-out-form').style.display = 'block';
+
+})
+
